5eb768a69bae432efcb88bf9427616d741ec262b
vincent anjiri <vincentanjiri12@gmail.com>
2024-10-13 14:00:05 +0300

    Update CSS and JSX files for various components, including MenuItem, Navbar, AboutUs, Chef, Header, and SpecialMenu, with new styles, layouts, and content.

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chief's word" />
+      <h1 className="headtext__cormorant">What We Believe in</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Aspernatur, dolores! Eius illum fugiat nulla distinctio tenetur vero
+          explicabo recusandae, fugit reiciendis nihil consequatur fuga culpa,
+          ipsa ab sint earum ut?
+        </p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
